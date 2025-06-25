836ce56a03b9647356baeb5ccddc4960c0fcf775
Mahaut Windal <mwindal@hotmail.com>
2025-06-25 13:44:49 +0200

    ajouts routes user et book

--- app.js
+++ app.js
@@ -3,28 +3,55 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const User = require('./model/user');
-// const { specs, swaggerUi } = require('./swagger');
+const Book = require('./model/book');
 
 dotenv.config();
 mongoose.set("strictQuery", false);
 
-// Connexion MongoDB
+// Connexion à MongoDB
 const mongoDB = process.env.MONGODB_URL;
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-  console.log('Connected to mongo server.');
-}
 
-// Initialisation Express
+async function main() {
+  try {
+    await mongoose.connect(mongoDB);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+main();
+
+// Création de l'application Express
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Exemple de route
 app.get("/", (req, res) => {
-  res.send("Hello from the API");
+  res.send("Bienvenue sur l'API !");
 });
 
-// Export de l'application Express
+// Routes USER
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.status(201).json(user);
+});
+
+// Routes BOOK
+app.get("/books", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
+});
+
+app.post("/books", async (req, res) => {
+  const book = new Book(req.body);
+  await book.save();
+  res.status(201).json(book);
+});
 module.exports = app;
