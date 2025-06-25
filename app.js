5894f17ce4f009054a2a38a2e5edb8a570d50dd3
Mahaut Windal <mwindal@hotmail.com>
2025-06-25 13:22:26 +0200

    reparation app

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-const User = require('./model/user')
+const User = require('./model/user');
 // const { specs, swaggerUi } = require('./swagger');
-dotenv.config();
 
+dotenv.config();
 mongoose.set("strictQuery", false);
 
-// Define the database URL to connect to.
+// Connexion MongoDB
 const mongoDB = process.env.MONGODB_URL;
-
-// Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
-
   console.log('Connected to mongo server.');
 }
+
+// Initialisation Express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Exemple de route
+app.get("/", (req, res) => {
+  res.send("Hello from the API");
+});
+
+// Export de l'application Express
+module.exports = app;
